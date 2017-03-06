5d8ae4abc599813d2f650d54c64f4bda13d041d1
satoshi.kobayashi@bizreach.co.jp <satoshi.kobayashi@BIZ2014-MAC0011.local>
2017-03-06 18:03:21 +0900

    finished the hero tutorial

--- example2_hero-tutorial/web/main.dart
+++ example2_hero-tutorial/web/main.dart
@@ -5,7 +5,7 @@ import 'package:angular2/router.dart';
 //import 'package:angular2/platform/common.dart';
 import 'package:example2_hero_tutorial/app_component.dart';
 import 'package:example2_hero_tutorial/in_memory_data_service.dart';
-import 'package:http/http.dart'; //import 'dart:html'; コレは間違いよ
+import 'package:http/http.dart';
 
 void main() {
   //http://stackoverflow.com/questions/37616385/angular2-routing-gives-404-when-reloading-page
